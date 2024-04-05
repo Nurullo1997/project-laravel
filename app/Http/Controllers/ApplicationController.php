a1e9ba1c2bb21248cad5217d1e96252655a28cad
Nurullo <nurullojakbaraliyev62@gmail.com>
2024-04-05 11:29:00 +0500

    Emayil yuborishlar

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -2,42 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ApplicationCreated;
 use App\Models\Application;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
 
 class ApplicationController extends Controller
 {
+
+
     public function store(Request $request)
     {
-
-        // Faylni tekshirish
         if ($request->hasFile('file')) {
-            // Fayl nomini olish
-            $fileName = $request->file('file')->getClientOriginalName();
-            
-            // Faylni diskka saqlash
-            $path = $request->file('file')->storeAs('public/files', $fileName);
-        } else {
-            // Fayl yuborilmaganligi haqida xabar
-            $path= null;
+
+            $name = $request->file('file')->getClientOriginalName();
+
+
+            $path = $request->file('file')->storeAs('public/file', $name);
         }
 
-        // Faylni validatsiya qilish
         $request->validate([
-            'subject' => 'required|string|max:255', // Subject ning maksimal uzunligi 255 belgi
-            'message' => 'required|string', // Message ning majburiy bo'lishi
-            'file' => 'nullable|file|mimes:jpeg,png,jpg,gif|max:2048', // Fayl formati, maksimum hajmi
+            'subject' => 'required|max:255',
+            'message' => 'required',
+            'file' => 'file|mimes:jpeg,png,jpg,gif',
         ]);
 
-        // Faylni bazaga saqlash
         $application = Application::create([
             'user_id' => auth()->user()->id,
             'subject' => $request->subject,
             'message' => $request->message,
-            'file_url' => $path,
+            'file_url' => $path ?? null,
         ]);
 
-        // Faylni bazaga saqlash muvaffaqiyatli bo'lganini xabar berish
-        return redirect()->back()->with('success', 'Ariza muvaffaqiyatli saqlandi!');
+
+
+
+        $manager = User::first();
+
+        Mail::to($manager)->send(new ApplicationCreated($application));
+
+        //Mail::to($request->user())->send(new ApplicationCreated($application));
+
+
+        return redirect()->back();
     }
 }
